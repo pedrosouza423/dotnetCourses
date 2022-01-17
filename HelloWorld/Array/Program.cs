6e003982b2410d530e9c0663931e7bada8ad602c
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2022-01-17 17:45:19 -0300

    Was studied about Strings, arrays

---
+++ HelloWorld/Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 25;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var names = new string[3] {"Pedro","Lucas","Luizinha"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+
+        }
+    }
+}
---
+++ HelloWorld/Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new string[3] { "Pedro", "Luizinha", "Lucas" };
+
+            var time = string.Join(",", names);
+            Console.WriteLine(time);
+
+            var firstName = "Pedro";
+            var lastName = "Souza";
+
+            var fullName = string.Join(' ', firstName, lastName);
+            Console.WriteLine(fullName);
+
+            var text = @"
+Hey, my name is Pedro
+I'm building this aplication
+I'm having a lot of fun learning english and .NET
+That's gonna be a great year
+The most important thing, you're done
+You're trying.
+I'm pround of you :D
+
+ _________________
+|HAVE A GREAT 2022|
+ -----------------";
+            Console.WriteLine(text);
+
+        }
+    }
+}
