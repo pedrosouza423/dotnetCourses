a4d26cdffe6b6b24e5d82b776ac3adf9571fc62e
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2022-01-20 13:59:49 -0300

    Finished 4 section

---
+++ HelloWorld/ValueRefereceTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValueRefereceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(String.Format("a:{0} b:{1}", a,b));
+
+            var array1 = new [] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+
+            Console.WriteLine(array1[0]);
+
+
+        }
+    }
+}
